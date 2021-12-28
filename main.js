422617fe6c8a466ae0863e45ab9709a9b9c2b4c7
sharatsagar <93038979+sharatsagar@users.noreply.github.com>
2021-12-28 18:03:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function addUser(){
+    player1_name=document.getElementById("player1").value; 
+    player2_name=document.getElementById("player2").value;                  
+    localStorage.setItem("player1name",player1_name);
+    localStorage.setItem("player2name",player2_name);
+    window.location="game_page.html";
+}
+player1_name=localStorage.getItem("player1name");
+player2_name=localStorage.getItem("player2name");
+player1_score=0;
+player2_score=0;
+document.getElementById("player1name").innerHTML=player1_name+":";
+document.getElementById("player2name").innerHTML=player2_name+":";
+document.getElementById("player1score").innerHTML=player1_score;
+document.getElementById("player2score").innerHTML=player2_score;
+document.getElementById("player?").innerHTML="question turn-"+player1_name;
+document.getElementById("playerA").innerHTML="answer turn-"+player2_name;
+function send(){
+    number1=document.getElementById("number1").value;
+    number2=document.getElementById("number2").value;
+    actual_anser=parseInt(number1) * parseInt(number2)
+    question_word="<h4 id='word_display'>q."+number1+"X"+number2+"</h4>";
+    input_box="<br> answer:<input id='input_check_box'>";
+    cheak_button="<br> <button class='btn btn-info'>check</button>";
+    row=question_word+input_box+cheak_button;
+    document.getElementById("output").innerHTML=row;
+}
+
